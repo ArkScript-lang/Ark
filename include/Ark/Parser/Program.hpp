326331b69911532e4197bf1572e63da2325eb082
Alexandre Plateau <lexplt.dev@gmail.com>
2019-02-27 13:54:35 +0100

    working on the program, need to add front() method in Parser

--- include/Ark/Parser/Program.hpp
+++ include/Ark/Parser/Program.hpp
@@ -5,6 +5,8 @@
 #include <iostream>
 #include <string>
 
+#include <Ark/Parser/Environment.hpp>
+#include <Ark/Parser/Parser.hpp>
 #include <Ark/Parser/Node.hpp>
 
 namespace Ark
@@ -17,12 +19,22 @@ namespace Ark
             Program();
             ~Program();
 
-            void add(const Node& node);
+            void feed(const std::string& file);
+            void execute(const Nodes& args={});
+
+            template <std::string FunctionName>
+            void loadFunction(Node::ProcType function)
+            {
+                m_global_env[FunctionName] = Node(function);
+            }
 
             friend std::ostream& operator<<(std::ostream& os, const Program& P);
         
         private:
-            Nodes m_program;
+            Node _execute(Node x, Environment* env);
+
+            Parser m_parser;
+            Environment m_global_env;
         };
     }
 }
