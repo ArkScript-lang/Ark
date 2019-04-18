531e7a3c78d447d7cf1c9a1e36a8560e4d326a41
Alexandre Plateau <lexplt.dev@gmail.com>
2019-04-18 13:53:46 +0200

    new FFI for the VM

--- include/Ark/VM/Value.hpp
+++ include/Ark/VM/Value.hpp
@@ -6,8 +6,7 @@
 #include <huge_number.hpp>
 #include <string>
 #include <cinttypes>
-
-#include <Ark/Lang/Node.hpp>
+#include <iostream>
 
 namespace Ark
 {
@@ -21,9 +20,11 @@ namespace Ark
         class Value
         {
         public:
-            using ValueType = std::variant<HugeNumber, std::string, PageAddr_t, NFT, Ark::Lang::Node::ProcType>;
+            using ProcType  = Value(*)(const std::vector<Value>&);
+            using Iterator = std::vector<Value>::const_iterator;
+            using ValueType = std::variant<HugeNumber, std::string, PageAddr_t, NFT, ProcType>;
 
-            Value();
+            Value(bool is_list=false);
             template <typename T> Value(const T& value);
             ~Value();
 
@@ -40,14 +41,28 @@ namespace Ark
             const std::string& string() const;
             const PageAddr_t pageAddr() const;
             const NFT nft() const;
-            const Ark::Lang::Node::ProcType proc() const;
+            const ProcType proc() const;
             const std::vector<Value>& list() const;
+
+            std::vector<Value>& list_ref();
+
+            void push_back(const Value& value);
+
+            friend std::ostream& operator<<(std::ostream& os, const Value& V);
+            friend inline bool operator==(const Value& A, const Value& B);
 
         private:
             ValueType m_value;
             std::vector<Value> m_list;
             bool m_is_list;
         };
+
+        inline bool operator==(const Value& A, const Value& B)
+        {
+            if (A.m_is_list || B.m_is_list)
+                return false;
+            return A.m_value == B.m_value;
+        }
     }
 }
 
