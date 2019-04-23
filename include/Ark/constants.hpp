b13411666d7acc5271724980a741245536d5e93d
Alexandre Plateau <lexplt.dev@gmail.com>
2019-04-23 17:36:48 +0200

    adding basic stuff

---
+++ include/Ark/constants.hpp
@@ -0,0 +1,14 @@
+#ifndef ark_constants
+#define ark_constants
+
+namespace Ark
+{
+    namespace Version
+    {
+        constexpr unsigned int Major = 0;
+        constexpr unsigned int Minor = 0;
+        constexpr unsigned int Patch = 1;
+    }
+}
+
+#endif  // ark_constants
