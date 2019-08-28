cf0325646facec80102d8a54ca4f58ccaa389176
Alexandre Plateau <lexplt.dev@gmail.com>
2019-08-28 17:05:38 +0200

    updating travis script

---
+++ .github/travis.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+cmake -H. -Bbuild -DCMAKE_C_COMPILER=${C_COMPILER} -DCMAKE_CXX_COMPILER=${CXX_COMPILER} -DCMAKE_BUILD_TYPE=Release -DARK_BUILD_EXE=1
+cmake --build build --config Release
+
+if [ -f build/Release/Ark ]; then
+    mv build/Release/Ark build/Ark
+fi
+build/Ark tests/unittest.ark --lib lib/
