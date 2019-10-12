515ddef41ad5233dc9acdb6dd7425ff436a1a30b
robert <rstefanic72@gmail.com>
2019-10-12 15:08:14 -0500

    Expanded REPL to feed compiler user entered code and feed it to the state. REPL can now catch errors and display them to the user. REPL will now also counter open braces to handle multi line programs that the user passes to the REPL.

--- include/Ark/REPL/Repl.hpp
+++ include/Ark/REPL/Repl.hpp
@@ -5,6 +5,9 @@
 #include <sstream>
 
 #include <Ark/Constants.hpp>
+#include <Ark/Compiler/Compiler.hpp>
+#include <Ark/VM/VM.hpp>
+#include <Ark/VM/State.hpp>
 
 namespace Ark {
     class Repl {
@@ -17,6 +20,7 @@ namespace Ark {
 
         inline void print_repl_header();
         int count_open_parentheses(std::string line);
+        int count_open_braces(std::string line);
     };
 }
 
--- src/REPL/Repl.cpp
+++ src/REPL/Repl.cpp
@@ -8,33 +8,65 @@ namespace Ark {
 
         print_repl_header();
 
+
         while(true)
         {
+            Ark::Compiler compiler(false);
+            Ark::State state(".");
+            Ark::VM vm(&state, m_options);
+            state.setDebug(false);
+
             std::stringstream code;
             int open_parentheses = 0;
+            int open_braces = 0;
+            bool new_command = true;
 
             std::cout << new_prompt;
-
             for(std::string line; std::getline(std::cin, line);)
             {
+                if (line.length() == 0)
+                {
+                    std::cout << (new_command ? new_prompt : continuing_prompt);
+                    continue;
+                }
+
                 if (line.compare("(quit)") == 0)
                 {
                     return;
                 }
 
-                code << line;
-
+                code << line << "\n";
                 open_parentheses += count_open_parentheses(line);
+                open_braces += count_open_braces(line);
 
-                if (open_parentheses == 0)
+                if (open_parentheses == 0 && open_braces == 0)
                 {
                     break;
                 }
 
+                new_command = false;
                 std::cout << continuing_prompt;
             }
 
-            std::cout << code.str() << "\n";
+            try
+            {
+                compiler.feed("{" + code.str() + "}");
+                compiler.compile();
+
+                state.feed(compiler.bytecode());
+                vm.run();
+            }
+            catch (const std::runtime_error& e) {
+                std::cerr << e.what() << std::endl;
+            }
+            catch (const std::exception& e)
+            {
+                std::cerr << e.what() << std::endl;
+            }
+            catch (...)
+            {
+                std::cerr << "Unknown lexer-parser-or-compiler error" << std::endl;
+            }
         }
     }
 
@@ -42,8 +74,8 @@ namespace Ark {
     {
         std::cout << "ArkScript REPL -- ";
         std::cout << "Version " << ARK_VERSION_MAJOR << "." << ARK_VERSION_MINOR << "." << ARK_VERSION_PATCH << " ";
-        std::cout << "[LICENSE: Mozilla Public License 2.0]\n";
-        std::cout << "Type \"(quit)\" to quit.\n";
+        std::cout << "[LICENSE: Mozilla Public License 2.0]" << std::endl;
+        std::cout << "Type \"(quit)\" to quit." << std::endl;
     }
 
     int Repl::count_open_parentheses(std::string line)
@@ -60,5 +92,21 @@ namespace Ark {
         }
 
         return open_parentheses;
+    }
+
+    int Repl::count_open_braces(std::string line)
+    {
+        int open_braces = 0;
+
+        for(char& c: line)
+        {
+            switch(c)
+            {
+                case '{': open_braces++; break;
+                case '}': open_braces--; break;
+            }
+        }
+
+        return open_braces;
     }
 }
