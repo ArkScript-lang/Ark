4f211629641214640b073f3741e1645cc7f3ded3
SuperFola <lexplt.dev@gmail.com>
2019-10-30 17:54:03 +0100

    fixing modules

--- modules/include/Ark/Module.hpp
+++ modules/include/Ark/Module.hpp
@@ -26,12 +26,12 @@ using Mapping_t = std::unordered_map<std::string, Value::ProcType>;
     #if __GNUC__ >= 4
         // GCC 4 has special keywords for showing/hidding symbols,
         // the same keyword is used for both importing and exporting
-        #define ARK_API_EXPORT __attribute__ ((__visibility__ ("default")))
-        #define ARK_API_IMPORT __attribute__ ((__visibility__ ("default")))
+        #define ARK_API_EXPORT extern "C" __attribute__ ((__visibility__ ("default")))
+        #define ARK_API_IMPORT extern "C" __attribute__ ((__visibility__ ("default")))
     #else
         // GCC < 4 has no mechanism to explicitely hide symbols, everything's exported
-        #define ARK_API_EXPORT
-        #define ARK_API_IMPORT
+        #define ARK_API_EXPORT extern "C"
+        #define ARK_API_IMPORT extern "C"
     #endif
 
     #define MAKE_ENTRY_POINT()
