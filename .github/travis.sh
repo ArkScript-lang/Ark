7a4a2f7426f59fb119d05914ac337079f05c7ac3
SuperFola <lexplt.dev@gmail.com>
2019-10-30 17:31:56 +0100

    updating travis.sh

--- .github/travis.sh
+++ .github/travis.sh
@@ -4,11 +4,11 @@ set -ev
 
 wget https://www.sfml-dev.org/files/SFML-2.5.1-sources.zip
 unzip SFML-2.5.1-sources.zip -d SFML
-cd SFML
+cd SFML/SFML-2.5.1/
 cmake -S. -Bbuild -DCMAKE_BUILD_TYPE=Release -DCMAKE_C_COMPILER=${C_COMPILER} -DCMAKE_CXX_COMPILER=${CXX_COMPILER}
 cmake --build build --config Release
 cmake --install build --config Release
-cd ..
+cd ../..
 
 cmake -S. -Bbuild -DCMAKE_C_COMPILER=${C_COMPILER} -DCMAKE_CXX_COMPILER=${CXX_COMPILER} -DCMAKE_BUILD_TYPE=Release -DARK_BUILD_EXE=1 -DARK_BUILD_MODULES=1
 cmake --build build --config Release
