5ec57ac4fb1e7db108cb3dd0495f785bc7adf7ca
Pierre Ph@rel <edo.tensei095@gmail.com>
2020-01-05 14:12:16 +0100

    msgpack : unpacking type automatic determination (type tests)

--- modules/msgpack/src/main.cpp
+++ modules/msgpack/src/main.cpp
@@ -37,41 +37,45 @@ namespace ArkMsgpack
     Value unpack(const std::vector<Value>& args)
     {
         //unpack(Value packed_str_buffer) and return an object unpacked 
-        if(args.size() != 2)
+        if(args.size() != 1)
             throw std::runtime_error("ArgError : This function must have 2 arguments");
         if(args[0].valueType() != ValueType::String && args[0].valueType() != ValueType::List)
             throw Ark::TypeError("The packed buffer must be a string or a list");
-        if(args[1].valueType() != ValueType::Number)
-            throw Ark::TypeError("The type index must be a number");        
+        //if(args[1].valueType() != ValueType::Number)
+        //    throw Ark::TypeError("The type index must be a number");        
         Value dst;
+        double ark_number;
+        std::string ark_string;
+        msgpack::object deserialized;
+        auto type_test = [&](void) {
+            std::string packed {static_cast<Value>(args[0]).string_ref()};
+            deserialized = msgpack::unpack(packed.data(), packed.size()).get();
+            try
+            {
+                deserialized.convert(ark_number);
+                dst = Value(ark_number);
+            }
+            catch(const std::bad_cast &e)
+            {
+                try
+                {
+                    deserialized.convert(ark_string);
+                    dst = Value(ark_string);
+                }
+                catch(const std::exception &e) {}
+            }
+        };
 
-        if(args[1] == 0)
-        {
-            std::string packed {static_cast<Value>(args[0]).string_ref()};
-            msgpack::object deserialized = msgpack::unpack(packed.data(), packed.size()).get();
-            double ark_number;
-            deserialized.convert(ark_number);
-            dst = Value(ark_number);
-        }
-        else if(args[1] == 1)
-        {
-            std::string packed {static_cast<Value>(args[0]).string_ref()};
-            msgpack::object deserialized = msgpack::unpack(packed.data(), packed.size()).get();
-            std::string ark_string;
-            deserialized.convert(ark_string);
-            dst = Value(ark_string);
-        }
-        else if(args[1] == 2)
+        if(args[0].valueType() == ValueType::List)
         {
             auto packed {static_cast<Value>(args[0]).list()};
             dst = list_unpacking(packed);
         }
+        else
+        {
+            type_test();
+        }
         return dst;
-    }
-    Value py(const std::vector<Value> &args)
-    {
-        std::vector<Value> v;
-        return v;
     }
 }
 ARK_API_EXPORT Mapping_t getFunctionsMapping()
@@ -79,7 +83,6 @@ ARK_API_EXPORT Mapping_t getFunctionsMapping()
     Mapping_t map;
 
     map["msgPack"] = ArkMsgpack::pack;
-    map["msgUnpack"] = ArkMsgpack::unpack; 
-    map["py"] = ArkMsgpack::py; 
+    map["msgUnpack"] = ArkMsgpack::unpack;  
     return map;
 }
