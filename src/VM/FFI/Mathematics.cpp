b804f2f375c699ad31d00f7fabf0040954f6f4c3
SuperFola <lexplt.dev@gmail.com>
2020-02-25 15:28:08 +0100

    adding better error messages in the ffi

--- src/VM/FFI/Mathematics.cpp
+++ src/VM/FFI/Mathematics.cpp
@@ -3,6 +3,7 @@
 
 #include <Ark/VM/FFI.hpp>
 
+#include <Ark/VM/FFIErrors.inl>
 #define FFI_Function(name) Value name(std::vector<Value>& n)
 
 namespace Ark::internal::FFI::Mathematics
@@ -10,9 +11,9 @@ namespace Ark::internal::FFI::Mathematics
     FFI_Function(exponential)
     {
         if (n.size() != 1)
-            throw std::runtime_error("exp can take only one argument, a Number");
+            throw std::runtime_error(MATH_ARITY("exp"));
         if (n[0].valueType() != ValueType::Number)
-            throw Ark::TypeError("Argument of exp must be of type Number");
+            throw Ark::TypeError(MATH_TE0("exp"));
         
         Value r(std::exp(n[0].number()));
         return r;
@@ -21,9 +22,9 @@ namespace Ark::internal::FFI::Mathematics
     FFI_Function(logarithm)
     {
         if (n.size() != 1)
-            throw std::runtime_error("log can take only one argument, a Number");
+            throw std::runtime_error(MATH_ARITY("log"));
         if (n[0].valueType() != ValueType::Number)
-            throw Ark::TypeError("Argument of log must be of type Number");
+            throw Ark::TypeError(MATH_TE0("log"));
         if (n[0].number() <= 0.0)
             throw std::runtime_error("Argument of log must be greater than 0");
         
@@ -34,9 +35,9 @@ namespace Ark::internal::FFI::Mathematics
     FFI_Function(ceil_)
     {
         if (n.size() != 1)
-            throw std::runtime_error("ceil can take only one argument, a Number");
+            throw std::runtime_error(MATH_ARITY("ceil"));
         if (n[0].valueType() != ValueType::Number)
-            throw Ark::TypeError("Argument of ceil must be of type Number");
+            throw Ark::TypeError(MATH_TE0("ceil"));
         
         Value r(std::ceil(n[0].number()));
         return r;
@@ -45,9 +46,9 @@ namespace Ark::internal::FFI::Mathematics
     FFI_Function(floor_)
     {
         if (n.size() != 1)
-            throw std::runtime_error("floor can take only one argument, a Number");
+            throw std::runtime_error(MATH_ARITY("floor"));
         if (n[0].valueType() != ValueType::Number)
-            throw Ark::TypeError("Argument of floor must be of type Number");
+            throw Ark::TypeError(MATH_TE0("floor"));
         
         Value r(std::floor(n[0].number()));
         return r;
@@ -56,9 +57,9 @@ namespace Ark::internal::FFI::Mathematics
     FFI_Function(round_)
     {
         if (n.size() != 1)
-            throw std::runtime_error("round can take only one argument, a Number");
+            throw std::runtime_error(MATH_ARITY("round"));
         if (n[0].valueType() != ValueType::Number)
-            throw Ark::TypeError("Argument of round must be of type Number");
+            throw Ark::TypeError(MATH_TE0("round"));
         
         Value r(std::round(n[0].number()));
         return r;
@@ -67,9 +68,9 @@ namespace Ark::internal::FFI::Mathematics
     FFI_Function(isnan_)
     {
         if (n.size() != 1)
-            throw std::runtime_error("isNaN can take only one argument, a value");
+            throw std::runtime_error(MATH_ARITY("isNaN"));
         if (n[0].valueType() != ValueType::Number)
-            return false;
+            return falseSym;
         
         return std::isnan(n[0].number()) ? trueSym : falseSym;
     }
@@ -77,9 +78,9 @@ namespace Ark::internal::FFI::Mathematics
     FFI_Function(isinf_)
     {
         if (n.size() != 1)
-            throw std::runtime_error("isInf can take only one argument, a value");
+            throw std::runtime_error(MATH_ARITY("isInf"));
         if (n[0].valueType() != ValueType::Number)
-            return false;
+            return falseSym;
         
         return std::isinf(n[0].number()) ? trueSym : falseSym;
     }
@@ -87,9 +88,9 @@ namespace Ark::internal::FFI::Mathematics
     FFI_Function(cos_)
     {
         if (n.size() != 1)
-            throw std::runtime_error("cos can take only one argument, a Number");
+            throw std::runtime_error(MATH_ARITY("cos"));
         if (n[0].valueType() != ValueType::Number)
-            throw Ark::TypeError("Argument of cos must be of type Number");
+            throw Ark::TypeError(MATH_TE0("cos"));
         
         Value r(std::cos(n[0].number()));
         return r;
@@ -98,9 +99,9 @@ namespace Ark::internal::FFI::Mathematics
     FFI_Function(sin_)
     {
         if (n.size() != 1)
-            throw std::runtime_error("sin can take only one argument, a Number");
+            throw std::runtime_error(MATH_ARITY("sin"));
         if (n[0].valueType() != ValueType::Number)
-            throw Ark::TypeError("Argument of sin must be of type Number");
+            throw Ark::TypeError(MATH_TE0("sin"));
         
         Value r(std::sin(n[0].number()));
         return r;
@@ -109,9 +110,9 @@ namespace Ark::internal::FFI::Mathematics
     FFI_Function(tan_)
     {
         if (n.size() != 1)
-            throw std::runtime_error("tan can take only one argument, a Number");
+            throw std::runtime_error(MATH_ARITY("tan"));
         if (n[0].valueType() != ValueType::Number)
-            throw Ark::TypeError("Argument of tan must be of type Number");
+            throw Ark::TypeError(MATH_TE0("tan"));
         
         Value r(std::tan(n[0].number()));
         return r;
@@ -120,9 +121,9 @@ namespace Ark::internal::FFI::Mathematics
     FFI_Function(acos_)
     {
         if (n.size() != 1)
-            throw std::runtime_error("arccos can take only one argument, a Number");
+            throw std::runtime_error(MATH_ARITY("arccos"));
         if (n[0].valueType() != ValueType::Number)
-            throw Ark::TypeError("Argument of arccos must be of type Number");
+            throw Ark::TypeError(MATH_TE0("arccos"));
         
         Value r(std::acos(n[0].number()));
         return r;
@@ -131,9 +132,9 @@ namespace Ark::internal::FFI::Mathematics
     FFI_Function(asin_)
     {
         if (n.size() != 1)
-            throw std::runtime_error("arcsin can take only one argument, a Number");
+            throw std::runtime_error(MATH_ARITY("arcsin"));
         if (n[0].valueType() != ValueType::Number)
-            throw Ark::TypeError("Argument of arcsin must be of type Number");
+            throw Ark::TypeError(MATH_TE0("arcsin"));
         
         Value r(std::asin(n[0].number()));
         return r;
@@ -142,9 +143,9 @@ namespace Ark::internal::FFI::Mathematics
     FFI_Function(atan_)
     {
         if (n.size() != 1)
-            throw std::runtime_error("arctan can take only one argument, a Number");
+            throw std::runtime_error(MATH_ARITY("arctan"));
         if (n[0].valueType() != ValueType::Number)
-            throw Ark::TypeError("Argument of arctan must be of type Number");
+            throw Ark::TypeError(MATH_TE0("arctan"));
         
         Value r(std::atan(n[0].number()));
         return r;
--- src/VM/FFI/System.cpp
+++ src/VM/FFI/System.cpp
@@ -3,6 +3,7 @@
 #include <cstdlib>
 #include <Ark/Constants.hpp>
 
+#include <Ark/VM/FFIErrors.inl>
 #define FFI_Function(name) Value name(std::vector<Value>& n)
 
 namespace Ark::internal::FFI::System
@@ -10,9 +11,9 @@ namespace Ark::internal::FFI::System
     FFI_Function(system_)
     {
         if (n.size() != 1)
-            throw std::runtime_error("system can take only one argument, a command");
+            throw std::runtime_error(SYS_SYS_ARITY);
         if (n[0].valueType() != ValueType::String)
-            throw Ark::TypeError("Argument of system must be of type String");
+            throw Ark::TypeError(SYS_SYS_TE0);
         
         #if ARK_ENABLE_SYSTEM != 0
             int output = std::system(n[0].string().c_str());
