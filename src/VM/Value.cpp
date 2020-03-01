07aaeae34626e3b736319ecf8c6bf90c16b79370
SuperFola <lexplt.dev@gmail.com>
2020-03-01 14:43:24 +0100

    better display of numbers

--- src/VM/Value.cpp
+++ src/VM/Value.cpp
@@ -111,13 +111,45 @@ namespace Ark::internal
 
     // --------------------------
 
+    int dec_places(double d)
+    {
+        constexpr double precision = 1e-7;
+        double temp = 0.0;
+        int decimal_places = 0;
+
+        do
+        {
+            d *= 10;
+            temp = d - static_cast<int>(d);
+            decimal_places++;
+        } while(temp > precision && decimal_places < std::numeric_limits<double>::digits10);
+        
+        return decimal_places;
+    }
+
+    int dig_places(double d)
+    {
+        int digit_places = 0;
+        int i = static_cast<int>(d);
+        while (i != 0)
+        {
+            digit_places++;
+            i /= 10;
+        }
+        return digit_places;
+    }
+
     std::ostream& operator<<(std::ostream& os, const Value& V)
     {
         switch (V.valueType())
         {
         case ValueType::Number:
-            os << Ark::Utils::toString(V.number());
+        {
+            double d = V.number();
+            os.precision(dig_places(d) + dec_places(d));
+            os << d;
             break;
+        }
         
         case ValueType::String:
             os << V.string();
