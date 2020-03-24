66e82834ce3a7d3ce2cfc2a8f6cecfe13795dd2d
SuperFola <lexplt.dev@gmail.com>
2020-03-24 16:30:45 +0100

    isolating inline code implementation of the compiler class in compiler.inl

--- include/Ark/Compiler/Compiler.hpp
+++ include/Ark/Compiler/Compiler.hpp
@@ -30,50 +30,36 @@ namespace Ark
     private:
         Ark::Parser m_parser;
         uint16_t m_options;
+        // tables: symbols, values, plugins and codes
         std::vector<std::string> m_symbols;
         std::vector<internal::CValue> m_values;
         std::vector<std::string> m_plugins;
         std::vector<std::vector<internal::Inst>> m_code_pages;
+            // we need a temp code pages for some compilations passes
         std::vector<std::vector<internal::Inst>> m_temp_pages;
 
         bytecode_t m_bytecode;
-
         unsigned m_debug;
 
-        inline std::vector<internal::Inst>& page(int i)
-        {
-            if (i >= 0)
-                return m_code_pages[i];
-            return m_temp_pages[-i - 1];
-        }
+        // helper functions to get a temp or finalized code page
+        inline std::vector<internal::Inst>& page(int i);
+        // checking if a symbol is an operator or a builtin
+        // because they are implemented the same way
+        inline std::optional<std::size_t> isOperator(const std::string& name);
+        inline std::optional<std::size_t> isBuiltin(const std::string& name);
 
-        inline std::optional<std::size_t> isOperator(const std::string& name)
-        {
-            auto it = std::find(internal::FFI::operators.begin(), internal::FFI::operators.end(), name);
-            if (it != internal::FFI::operators.end())
-                return std::distance(internal::FFI::operators.begin(), it);
-            return {};
-        }
-
-        inline std::optional<std::size_t> isBuiltin(const std::string& name)
-        {
-            auto it = std::find_if(internal::FFI::builtins.begin(), internal::FFI::builtins.end(),
-            [&name](const std::pair<std::string, internal::Value>& element) -> bool {
-                return name == element.first;
-            });
-            if (it != internal::FFI::builtins.end())
-                return std::distance(internal::FFI::builtins.begin(), it);
-            return {};
-        }
-
+        // to compile a single node, recursively
         void _compile(Ark::internal::Node x, int p);
+        // register a symbol/value/plugin in its own table
         std::size_t addSymbol(const std::string& sym);
         std::size_t addValue(Ark::internal::Node x);
         std::size_t addValue(std::size_t page_id);
         void addPlugin(Ark::internal::Node x);
-
+        // push a number on stack (need 2 )
         void pushNumber(uint16_t n, std::vector<internal::Inst>* page=nullptr);
     };
+
+    #include "Compiler.inl"
 }
 
 #endif
