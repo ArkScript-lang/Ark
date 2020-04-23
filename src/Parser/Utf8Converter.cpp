3fbba3eb1f04ff32f557f5f3b491219421a09d4f
SuperFola <lexplt.dev@gmail.com>
2020-04-23 17:26:13 +0200

    setting the converters from/to utf8 as static to avoid creating them to many times

--- src/Parser/Utf8Converter.cpp
+++ src/Parser/Utf8Converter.cpp
@@ -9,14 +9,14 @@ namespace Ark::internal
 {
     std::string ws_to_utf8(const std::wstring& s)
     {
-        std::wstring_convert<std::codecvt_utf8_utf16<wchar_t>> converter;
+        static std::wstring_convert<std::codecvt_utf8_utf16<wchar_t>> converter;
         std::string narrow = converter.to_bytes(s);
         return narrow;
     }
 
     std::wstring utf8_to_ws(const std::string& utf8)
     {
-        std::wstring_convert<std::codecvt_utf8_utf16<wchar_t>> converter;
+        static std::wstring_convert<std::codecvt_utf8_utf16<wchar_t>> converter;
         std::wstring wide = converter.from_bytes(utf8);
         return wide;
     }
