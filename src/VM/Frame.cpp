9dba71939dfa344592f327bfe7680081c8782ea5
SuperFola <lexplt.dev@gmail.com>
2020-05-04 12:27:52 +0200

    smaller frames by using uint16_t instead of plain std::size_t

--- src/VM/Frame.cpp
+++ src/VM/Frame.cpp
@@ -4,14 +4,12 @@ namespace Ark::internal
 {
     Frame::Frame() :
         m_addr(0), m_page_addr(0), m_new_pp(0),
-        m_stack(ARK_MAX_STACK_SIZE), m_i(0),
-        m_scope_to_delete(0)
+        m_i(0), m_scope_to_delete(0)
     {}
 
-    Frame::Frame(std::size_t caller_addr, std::size_t caller_page_addr, std::size_t new_pp) :
+    Frame::Frame(uint16_t caller_addr, uint16_t caller_page_addr, uint16_t new_pp) :
         m_addr(caller_addr), m_page_addr(caller_page_addr), m_new_pp(new_pp),
-        m_stack(ARK_MAX_STACK_SIZE), m_i(0),
-        m_scope_to_delete(0)
+        m_i(0), m_scope_to_delete(0)
     {}
 
     std::ostream& operator<<(std::ostream& os, const Frame& F)
