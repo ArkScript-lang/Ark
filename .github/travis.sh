2ffe741112ed26b58cc55530c69e2f8aeb4a5b37
SuperFola <lexplt.dev@gmail.com>
2020-06-25 23:23:14 +0200

    updating build script for travis

--- .github/travis.sh
+++ .github/travis.sh
@@ -3,7 +3,8 @@
 set -ev
 
 cmake . -Bbuild -DCMAKE_C_COMPILER=${C_COMPILER} -DCMAKE_CXX_COMPILER=${CXX_COMPILER} -DCMAKE_BUILD_TYPE=Release -DARK_BUILD_EXE=1 -DARK_BUILD_BASE_MODULES=1
-cmake --build build --config Release
+cmake --build build --config Release --target Ark
+cmake --build build --config Release --target console
 
 if [ -f build/Release/Ark ]; then
     mv build/Release/Ark build/Ark
