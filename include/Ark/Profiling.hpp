b4779f13222e116886b4afc32317f8ad97a3f10b
Alexandre Plateau <lexplt.dev@gmail.com>
2020-10-26 11:43:07 +0100

    fixing Profiling.hpp: wrong preprocessor directive

--- include/Ark/Profiling.hpp
+++ include/Ark/Profiling.hpp
@@ -3,8 +3,17 @@
 
 #include <Ark/Constants.hpp>
 
-#if defined(ARK_PROFILER) && ARK_PROFILER != 0 
-    #include <coz.h>
+#if defined(ARK_PROFILER)
+    #if ARK_PROFILER==1
+        #include <coz.h>
+    #else
+        // define the coz macro but don't add content to them
+        // so that we can "use" them but they won't take effect
+        #define COZ_PROGRESS_NAMED(name)
+        #define COZ_PROGRESS
+        #define COZ_BEGIN(name)
+        #define COZ_END(name)
+    #endif
 #else
     // define the coz macro but don't add content to them
     // so that we can "use" them but they won't take effect
