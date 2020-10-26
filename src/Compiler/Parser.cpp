3bb2250723b3dafa8ccb837ae33824bc773b6a82
SuperFola <lexplt.dev@gmail.com>
2020-10-26 11:24:48 +0100

    the parser can now handle (let a b.c)

--- src/Compiler/Parser.cpp
+++ src/Compiler/Parser.cpp
@@ -174,27 +174,17 @@ namespace Ark
                         if (tokens.front().token != ")")
                             block.push_back(parse(tokens));
                     }
-                    else if (token.token == "let")
+                    else if (token.token == "let" || token.token == "mut")
                     {
                         auto temp = tokens.front();
                         // parse identifier
                         if (temp.type == TokenType::Identifier)
                             block.push_back(atom(nextToken(tokens)));
                         else
-                            throwParseError("missing identifier to define a constant, after keyword `let'", temp);
+                            throwParseError(std::string("missing identifier to define a ") + (token.token == "let" ? "constant" : "variable") + ", after keyword `" + token.token + "'", temp);
                         // value
-                        block.push_back(parse(tokens));
-                    }
-                    else if (token.token == "mut")
-                    {
-                        auto temp = tokens.front();
-                        // parse identifier
-                        if (temp.type == TokenType::Identifier)
-                            block.push_back(atom(nextToken(tokens)));
-                        else
-                            throwParseError("missing identifier to define a variable, after keyword `mut'", temp);
-                        // value
-                        block.push_back(parse(tokens));
+                        while (tokens.front().token != ")")
+                            block.push_back(parse(tokens, /* authorize_capture */ false, /* authorize_field_read */ true));
                     }
                     else if (token.token == "set")
                     {
@@ -205,7 +195,8 @@ namespace Ark
                         else
                             throwParseError("missing identifier to assign a value to, after keyword `set'", temp);
                         // value
-                        block.push_back(parse(tokens));
+                        while (tokens.front().token != ")")
+                            block.push_back(parse(tokens, /* authorize_capture */ false, /* authorize_field_read */ true));
                     }
                     else if (token.token == "fun")
                     {
