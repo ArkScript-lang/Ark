703a5e10380c8516445193aa5175f9ba762873ef
SuperFola <lexplt.dev@gmail.com>
2020-11-22 12:20:18 +0100

    fixing compiler

--- src/Compiler/Compiler.cpp
+++ src/Compiler/Compiler.cpp
@@ -528,7 +528,7 @@ namespace Ark
                     default:
                         throwCompilerError("can not create a chained expression (of length " + Utils::toString(exp_count) +
                             ") for operator `" + Builtins::operators[static_cast<std::size_t>(op_inst - Instruction::FIRST_OPERATOR)] +
-                            "'. You most likely forgot a `)'.");
+                            "'. You most likely forgot a `)'.", x);
                 }
             }
         }
