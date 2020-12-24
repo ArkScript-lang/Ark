c580e2edb318829cc0fb301cf3ded8cc02886224
Alexandre Plateau <lexplt.dev@gmail.com>
2020-12-24 12:04:54 +0100

    fixing ARK_PROFILER define

--- include/Ark/Profiling.hpp
+++ include/Ark/Profiling.hpp
@@ -3,17 +3,8 @@
 
 #include <Ark/Constants.hpp>
 
-#if defined(ARK_PROFILER)
-    #if ARK_PROFILER==1
-        #include <coz.h>
-    #else
-        // define the coz macro but don't add content to them
-        // so that we can "use" them but they won't take effect
-        #define COZ_PROGRESS_NAMED(name)
-        #define COZ_PROGRESS
-        #define COZ_BEGIN(name)
-        #define COZ_END(name)
-    #endif
+#if ARK_PROFILER == 1
+    #include <coz.h>
 #else
     // define the coz macro but don't add content to them
     // so that we can "use" them but they won't take effect
