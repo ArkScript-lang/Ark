592b9bfa058d6bf0e9ccbb32a677648376cb02f8
SuperFola <lexplt.dev@gmail.com>
2020-12-25 22:38:43 +0100

    trying to introduce internal symbol references

--- include/Ark/VM/VM.hpp
+++ include/Ark/VM/VM.hpp
@@ -155,6 +155,13 @@ namespace Ark
         // locals related
 
         /**
+         * @brief Pop a value from the stack and resolve it if possible, then return it
+         * 
+         * @return internal::Value* 
+         */
+        inline internal::Value* popAndResolveAsPtr();
+
+        /**
          * @brief Find the nearest variable of a given id
          * 
          * @param id the id to find
