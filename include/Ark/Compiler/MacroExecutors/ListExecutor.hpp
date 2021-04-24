a758b5c7b921895b595b3e4c1e0e4d431c07a726
Ray Alovera <ralovera@stackify.com>
2021-04-24 14:25:41 +0800

    first commit

---
+++ include/Ark/Compiler/MacroExecutors/ListExecutor.hpp
@@ -0,0 +1,19 @@
+#ifndef ARK_COMPILER_LISTEXECUTOR_HPP
+#define ARK_COMPILER_LISTEXECUTOR_HPP   
+#include <Ark/Compiler/MacroExecutor.hpp>
+#include <unordered_map>
+#include <vector>
+#include <Ark/Compiler/Node.hpp>
+namespace Ark::internal {
+    class ListExecutor : public MacroExecutor{
+        public:
+            void execute(std::function<Node*(const std::string& name)> find_nearest_macro, 
+                                std::function<void(Node &node)> registerMacro,
+                                std::function<bool(const Node& node)> isTruthy,
+                                std::function<Node(Node& node, bool is_not_body)> evaluate,
+                                Node &node);
+    };
+
+}
+
+#endif
