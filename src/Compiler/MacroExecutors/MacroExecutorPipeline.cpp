9696dded8a9f3ef18e18d842a4a14a061f471805
Ray Alovera <ralovera@stackify.com>
2021-05-02 18:49:38 +0800

    use const references
    Former-commit-id: 1136cc4cfbdb93adb07cc0f3b1ddb48b113239c0

--- src/Compiler/MacroExecutors/MacroExecutorPipeline.cpp
+++ src/Compiler/MacroExecutors/MacroExecutorPipeline.cpp
@@ -3,24 +3,18 @@
 namespace Ark::internal
 {
     MacroExecutorPipeline::MacroExecutorPipeline(
-        std::function<Node *(const std::string &name)> find_nearest_macro,
-        std::function<void(Node &node)> registerMacro,
-        std::function<bool(const Node &node)> isTruthy,
-        std::function<Node(Node &node, bool is_not_body)> evaluate,
-        std::function<void(const std::unordered_map<std::string, Node> &, Node &, Node *)> apply_to,
-        std::function<void(const std::string &message, const Node &node)> throwMacroProcessingError,
-        std::function<void(Node &node)> func_execute,
+        std::function<Node *(const std::string &name)> const& find_nearest_macro,
+        std::function<void(Node &node)> const& registerMacro,
+        std::function<bool(const Node &node)> const& isTruthy,
+        std::function<Node(Node &node, bool is_not_body)> const& evaluate,
+        std::function<void(const std::unordered_map<std::string, Node> &, Node &, Node *)> const& apply_to,
+        std::function<void(const std::string &message, const Node &node)> const& throwMacroProcessingError,
+        std::function<void(Node &node)> const& func_execute,
         std::vector<std::shared_ptr<MacroExecutor>> executors
-    )
+    ) : m_find_nearest_macro(find_nearest_macro), m_registerMacro(registerMacro),
+        m_isTruthy(isTruthy), m_evaluate(evaluate), m_apply_to(apply_to), m_throwMacroProcessingError(throwMacroProcessingError),
+        m_func_execute(func_execute)
     {
-        this->find_nearest_macro = find_nearest_macro;
-        this->registerMacro = registerMacro;
-        this->isTruthy = isTruthy;
-        this->evaluate = evaluate;
-        this->apply_to = apply_to;
-        this->throwMacroProcessingError = throwMacroProcessingError;
-        this->func_execute = func_execute;
-
        m_executors = executors;
     }
     void MacroExecutorPipeline::execute(Node &node)
@@ -28,13 +22,13 @@ namespace Ark::internal
         for (std::shared_ptr<MacroExecutor> executor : m_executors)
         {
             executor->execute(
-                find_nearest_macro,
-                registerMacro,
-                isTruthy,
-                evaluate,
-                apply_to,
-                throwMacroProcessingError,
-                func_execute,
+                m_find_nearest_macro,
+                m_registerMacro,
+                m_isTruthy,
+                m_evaluate,
+                m_apply_to,
+                m_throwMacroProcessingError,
+                m_func_execute,
                 node);
         }
     }
