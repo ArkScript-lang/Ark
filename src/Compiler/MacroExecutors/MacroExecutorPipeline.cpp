5d174d585ffa5316a45fd40510276f6400526dd1
Ray Alovera <rakista112@gmail.com>
2021-05-10 16:00:44 +0800

    - change type in loop
    Former-commit-id: 01f4e96cc51641ca4aecbaae2a4f18159b3e9155

--- src/Compiler/MacroExecutors/MacroExecutorPipeline.cpp
+++ src/Compiler/MacroExecutors/MacroExecutorPipeline.cpp
@@ -10,7 +10,7 @@ namespace Ark::internal
     }
     void MacroExecutorPipeline::execute(Node& node)
     {
-        for (std::shared_ptr<MacroExecutor> executor : m_executors)
+        for (const std::shared_ptr<MacroExecutor>& executor : m_executors)
         {
             executor->execute(node);
         }
