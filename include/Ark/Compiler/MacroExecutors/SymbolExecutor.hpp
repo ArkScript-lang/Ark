6a9831d4ec08b6d6e256afbeae6ec1f4cfa5a2e9
Ray Alovera <rakista112@gmail.com>
2021-05-11 18:39:54 +0800

    - remove unused headers

--- include/Ark/Compiler/MacroExecutors/SymbolExecutor.hpp
+++ include/Ark/Compiler/MacroExecutors/SymbolExecutor.hpp
@@ -12,8 +12,6 @@
 #ifndef ARK_COMPILER_SYMBOLEXECUTOR_HPP
 #define ARK_COMPILER_SYMBOLEXECUTOR_HPP
 #include <Ark/Compiler/MacroExecutor.hpp>
-#include <unordered_map>
-#include <vector>
 #include <Ark/Compiler/Node.hpp>
 
 namespace Ark::internal
