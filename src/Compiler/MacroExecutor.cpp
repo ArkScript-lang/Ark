d0e6464adb1cd634871a26c8b95b4cb146496271
Ray Alovera <rakista112@gmail.com>
2021-05-12 14:42:33 +0800

    - remove extra qualifier
    Former-commit-id: 33856deaf1151d0becb8fd73f698e30f6a1b33fa

--- src/Compiler/MacroExecutor.cpp
+++ src/Compiler/MacroExecutor.cpp
@@ -9,33 +9,33 @@ namespace Ark::internal
 
     Node* MacroExecutor::m_find_nearest_macro(const std::string& name)
     {
-        return MacroExecutor::m_macroprocessor->find_nearest_macro(name);
+        return m_macroprocessor->find_nearest_macro(name);
     }
 
     void MacroExecutor::m_registerMacro(Node& node)
     {
-        MacroExecutor::m_macroprocessor->registerMacro(node);
+        m_macroprocessor->registerMacro(node);
     }
 
     bool MacroExecutor::m_isTruthy(const Node& node)
     {
-        return MacroExecutor::m_macroprocessor->isTruthy(node);
+        return m_macroprocessor->isTruthy(node);
     }
 
     Node MacroExecutor::m_evaluate(Node& node, bool is_not_body)
     {
-        return MacroExecutor::m_macroprocessor->evaluate(node, is_not_body);
+        return m_macroprocessor->evaluate(node, is_not_body);
     }
     void MacroExecutor::m_apply_to(const std::unordered_map<std::string, Node>& map, Node& target, Node* parent)
     {
-        MacroExecutor::m_macroprocessor->apply_to(map, target, parent);
+        m_macroprocessor->apply_to(map, target, parent);
     }
     void MacroExecutor::m_throwMacroProcessingError(const std::string& message, const Node& node)
     {
-        MacroExecutor::m_macroprocessor->throwMacroProcessingError(message, node);
+        m_macroprocessor->throwMacroProcessingError(message, node);
     }
     void MacroExecutor::m_execute_proxy(Node& node)
     {
-        MacroExecutor::m_macroprocessor->execute(node);
+        m_macroprocessor->execute(node);
     }
 }
