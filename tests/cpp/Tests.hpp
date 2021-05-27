43bfbba5d23b7e13f8301c9d41eccffd6190943a
Alexandre Plateau <lexplt.dev@gmail.com>
2021-05-27 15:41:18 +0200

    fixing header file Tests.hpp

--- tests/cpp/Tests.hpp
+++ tests/cpp/Tests.hpp
@@ -3,16 +3,16 @@
 
 #include <iostream>
 
-#define CHECK_VM_RUN(vm)  \
-    if (auto code = vm.run(); code != 0) {  \
+#define CHECK_VM_RUN(vm)                                   \
+    if (auto code = vm.run(); code != 0) {                 \
         std::cerr << "vm.run() returned " << code << "\n"; \
-        return 1; \
+        return 1;                                          \
     }
 
-#define CHECK_VALUE_NUMBER(val, expected) \
-    if (val.valueType() != Ark::ValueType::Number || val.number() != (expected)) { \
-        std::cerr << #val << " isn't a Number or doesn't have the value " << #expected << "\n";
-        return 1;
+#define CHECK_VALUE_NUMBER(val, expected)                                                       \
+    if (val.valueType() != Ark::ValueType::Number || val.number() != (expected)) {              \
+        std::cerr << #val << " isn't a Number or doesn't have the value " << #expected << "\n"; \
+        return 1;                                                                               \
     }
 
 #define RETURN_PASSED() return 0;
