a4057b1cbf5ea3d80eded0638b4196c2e6685082
Alexandre Plateau <lexplt.dev@gmail.com>
2021-06-07 20:56:34 +0200

    fixing errors' context creation

--- src/Compiler/makeErrorCtx.cpp
+++ src/Compiler/makeErrorCtx.cpp
@@ -8,10 +8,12 @@
 
 namespace Ark::internal
 {
-    void makeContext(std::ostream& os, const std::string& code, std::size_t line, std::size_t col_start, std::size_t col_end)
+    void makeContext(std::ostream& os, const std::string& code, std::size_t line, std::size_t col_start, std::size_t sym_size)
     {
         std::vector<std::string> ctx = Utils::splitString(code, '\n');
         int iline = static_cast<int>(line);
+
+        std::size_t col_end = std::min(col_start + sym_size, ctx[line].size());
 
         for (int i = 3; i > -3; --i)
         {
@@ -22,12 +24,14 @@ namespace Ark::internal
             if (i == 0)  // line of the error
             {
                 os << "      | ";
-                // padding of spaces
-                for (std::size_t j = 0; (ssize > col_start) ? false : (j < col_start); ++j)
-                    os << " ";
-                // show the error
-                for (std::size_t j = 0; (ssize > col_start) ? (j < col_end) : (j < ssize); ++j)
-                    os << "^";
+
+                for (std::size_t j = 0; (sym_size > col_start) ? false : (j < col_start); ++j)
+                {
+                    if (j < col_start)  // padding of spaces
+                        os << " ";
+                    else if (j >= col_start && j <= col_end)  // underline the error
+                        os << "^";
+                }
                 os << "\n";
             }
         }
@@ -47,7 +51,7 @@ namespace Ark::internal
             ssize = node.string().size();
 
         if (node.filename() != ARK_NO_NAME_FILE)
-            makeContext(ss, Utils::readFile(node.filename()), node.line(), node.col(), ctx[node.line()].size());
+            makeContext(ss, Utils::readFile(node.filename()), node.line(), node.col(), ssize);
 
         return ss.str();
     }
@@ -55,12 +59,7 @@ namespace Ark::internal
     std::string makeTokenBasedErrorCtx(const std::string& match, std::size_t line, std::size_t col, const std::string& code)
     {
         std::stringstream ss;
-        ss << message << "\n";
-        if (??? != ARK_NO_NAME_FILE)
-            ss << "In file " << ??? << "\n";
-        ss << "On line " << (line + 1) << ":" << col << ", got TokenType::";
-        ss << tokentype_string[static_cast<unsigned>(token.type)] << "\n";
-
+        ss << "On line " << (line + 1) << ":" << col << "\n";
         makeContext(ss, code, line, col, match.size());
 
         return ss.str();
