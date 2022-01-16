f0b7ed7de1f6bb9984a82f0a78269e8b9d015ede
Alexandre Plateau <lexplt.dev@gmail.com>
2022-01-16 18:47:35 +0100

    improving the type checker
    Former-commit-id: 7d5748a7e3eabb3a8eb48f39622eb32388fea33d

--- include/Ark/TypeChecker.hpp
+++ include/Ark/TypeChecker.hpp
@@ -2,7 +2,7 @@
  * @file TypeChecker.hpp
  * @author Alexandre Plateau (lexplt.dev@gmail.com)
  * @brief 
- * @version 0.1
+ * @version 0.2
  * @date 2022-01-16
  * 
  * @copyright Copyright (c) 2022
@@ -19,32 +19,32 @@
 namespace Ark::internal::types
 {
     /**
+     * @brief A type definition within a contract
+     * 
+     */
+    struct Typedef
+    {
+        std::string_view name;
+        std::vector<ValueType> types;
+        bool variadic;
+
+        Typedef(std::string_view name, ValueType type, bool variadic = false) :
+            name(name), variadic(variadic)
+        {
+            types.emplace_back(type);
+        }
+
+        Typedef(std::string_view name, const std::vector<ValueType>& types, bool variadic = false) :
+            name(name), types(types), variadic(variadic)
+        {}
+    };
+
+    /**
      * @brief A contract is a list of typed arguments that a function can follow
      * 
      */
     struct Contract
     {
-        /**
-         * @brief A type definition within a contract
-         * 
-         */
-        struct Typedef
-        {
-            std::string_view name;
-            std::vector<ValueType> types;
-            bool variadic;
-
-            Typedef(std::string_view name, ValueType type, bool variadic = false) :
-                name(name), variadic(variadic)
-            {
-                types.emplace_back(type);
-            }
-
-            Typedef(std::string_view name, const std::vector<ValueType>& types, bool variadic = false) :
-                name(name), types(types), variadic(variadic)
-            {}
-        };
-
         std::vector<Typedef> arguments;
     };
 
@@ -52,18 +52,18 @@ namespace Ark::internal::types
      * @brief Define all the types we can use in contracts
      * 
      */
-    const std::vector<ValueType> AnyType {{
-        ValueType::List,
-        ValueType::Number,
-        ValueType::String,
-        ValueType::PageAddr,
-        ValueType::CProc,
-        ValueType::Closure,
-        ValueType::User,
-        ValueType::Nil,
-        ValueType::True,
-        ValueType::False
-    }};
+    const std::vector<ValueType> AnyType {
+        { ValueType::List,
+          ValueType::Number,
+          ValueType::String,
+          ValueType::PageAddr,
+          ValueType::CProc,
+          ValueType::Closure,
+          ValueType::User,
+          ValueType::Nil,
+          ValueType::True,
+          ValueType::False }
+    };
 
     /**
      * @brief Applies type checks and arity check to the provided argument, following a set of contracts the function can follow
@@ -73,7 +73,12 @@ namespace Ark::internal::types
      * @param provided_arguments 
      * @return std::size_t the index of the contract used
      */
-    std::size_t typeChecker(std::string_view funcname, const std::vector<Contract>& contracts, const std::vector<Value>& provided_arguments);
+    std::size_t checker(std::string_view funcname, const std::vector<Contract>& contracts, const std::vector<Value>& provided_arguments);
+
+    inline std::size_t checker(std::string_view funcname, const Contract& contract, const std::vector<Value>& provided_arguments)
+    {
+        return checker(funcname, {{ contract }}, provided_arguments);
+    }
 }
 
 #endif
--- src/arkreactor/TypeChecker.cpp
+++ src/arkreactor/TypeChecker.cpp
@@ -1,16 +1,67 @@
 #include <Ark/TypeChecker.hpp>
 
 #include <limits>
+#include <numeric>
 #include <algorithm>
+#define NOMINMAX
 #include <termcolor/termcolor.hpp>
 
 #include <Ark/Exceptions.hpp>
 
 namespace Ark::internal::types
 {
-    // TODO create a function to generate the basic skeleton of an error message
+    std::string typeListToString(const std::vector<ValueType>& types)
+    {
+        if (types.size() == AnyType.size())  // without more checks we'll assume they are the same
+            return "any";
 
-    std::size_t typeChecker(std::string_view funcname, const std::vector<Contract>& contracts, const std::vector<Value>& provided_arguments)
+        return std::accumulate(
+            std::next(types.begin()),
+            types.end(),
+            std::string(""),
+            [](const std::string& a, ValueType type) -> std::string {
+                return a + ", " + types_to_str[static_cast<std::size_t>(type)];
+            });
+    }
+
+    void displayContract(const Contract& contract, const std::vector<Value>& args)
+    {
+        for (std::size_t i = 0, end = contract.arguments.size(); i < end; ++i)
+        {
+            const Typedef& td = contract.arguments[i];
+            std::string arg_str = typeListToString(td.types);
+
+            std::cout << "  -> " << (td.variadic ? "variadic " : "")
+                      << termcolor::green << td.name << termcolor::reset << " (" << arg_str << ") ";
+
+            if (td.variadic && i < args.size())
+            {
+                // variadic argument in contract and enough provided arguments
+                std::size_t bad_type = 0;
+                for (std::size_t j = i, args_end = args.size(); j < args_end; ++j)
+                {
+                    if (std::find(td.types.begin(), td.types.end(), args[j].valueType()) == td.types.end())
+                        bad_type++;
+                }
+
+                if (bad_type)
+                    std::cout << termcolor::red << bad_type << termcolor::reset
+                              << " argument" << (bad_type > 1 ? "s" : "") << " do not match";
+            }
+            else
+            {
+                // provided argument but wrong type
+                if (i < args.size() && std::find(td.types.begin(), td.types.end(), args[i].valueType()) == td.types.end())
+                    std::cout << "was of type " << termcolor::yellow << types_to_str[static_cast<std::size_t>(args[i].valueType())];
+                // non-provided argument
+                else if (i >= args.size())
+                    std::cout << termcolor::red << "was not provided";
+            }
+            std::cout << termcolor::reset << "\n";
+        }
+    }
+
+    std::size_t checker(std::string_view funcname, const std::vector<Contract>& contracts, const std::vector<Value>& provided_arguments)
     {
         for (std::size_t i = 0, end = contracts.size(); i < end; ++i)
         {
@@ -23,36 +74,39 @@ namespace Ark::internal::types
             if (c.arguments.empty() && provided_arguments.empty())
                 return i;
 
-            if (provided_arguments.size() == c.arguments.size() && !c.arguments.back().variadic)
+            if ((provided_arguments.size() == c.arguments.size() && !c.arguments.back().variadic) ||
+                (provided_arguments.size() >= c.arguments.size() - 1 && c.arguments.back().variadic))
             {
-                // number of argument correct, no variadic at the end
-                for (std::size_t j = 0, arg_end = c.arguments.size(); j < arg_end; ++j)
-                {
-                    const Contract::Typedef& td = c.arguments[j];
-                    if (std::find(td.types.begin(), td.types.end(), provided_arguments[j].valueType()) == td.types.end())
-                        throw TypeError("smh");  // TODO generate the message
-                }
-
-                return i;
-            }
-            else if (provided_arguments.size() >= c.arguments.size() - 1 && c.arguments.back().variadic)
-            {
-                // number of arguments is at least the number of required arguments (not counting variadic)
-                // check the regular arguments first
                 std::size_t j = 0;
-                for (std::size_t arg_end = c.arguments.size() - 1; j < arg_end; ++j)
+                std::size_t arg_end = c.arguments.size();
+
+                // need to check only the first arguments in case of a variadic type definition in the contract
+                if (c.arguments.back().variadic)
+                    arg_end--;
+
+                for (; j < arg_end; ++j)
                 {
-                    const Contract::Typedef& td = c.arguments[j];
+                    const Typedef& td = c.arguments[j];
                     if (std::find(td.types.begin(), td.types.end(), provided_arguments[j].valueType()) == td.types.end())
-                        throw TypeError("smh");  // TODO generate the message
+                    {
+                        displayContract(c, provided_arguments);
+                        throw Error("");
+                    }
                 }
 
-                const Contract::Typedef& var_td = c.arguments[j];
-                // check the variadic arguments
-                for (std::size_t arg_end = provided_arguments.size(); j < arg_end; ++j)
+                // check the variadic part if any
+                if (c.arguments.back().variadic)
                 {
-                    if (std::find(var_td.types.begin(), var_td.types.end(), provided_arguments[j].valueType()) == var_td.types.end())
-                        throw TypeError("smh");  // TODO generate the message
+                    const Typedef& var_td = c.arguments[j];
+                    // check the variadic arguments
+                    for (std::size_t provided_end = provided_arguments.size(); j < provided_end; ++j)
+                    {
+                        if (std::find(var_td.types.begin(), var_td.types.end(), provided_arguments[j].valueType()) == var_td.types.end())
+                        {
+                            displayContract(c, provided_arguments);
+                            throw Error("");
+                        }
+                    }
                 }
 
                 return i;
@@ -60,18 +114,40 @@ namespace Ark::internal::types
         }
 
         // no match, the user most likely provided the wrong number of arguments
-        std::cout << termcolor::green << funcname << termcolor::reset;
+        std::cout << termcolor::green << funcname << termcolor::reset << " expected ";
 
         if (contracts.size() == 1)
         {
             std::size_t expected_argc = contracts[0].arguments.size();
-            std::cout << " expected " << expected_argc << " argument" << (expected_argc > 1 ? "s" : "")
-                      << " but got " << provided_arguments.size() << "\n";
-            // TODO display contract
+            std::cout << termcolor::yellow << expected_argc << termcolor::reset
+                      << " argument" << (expected_argc > 1 ? "s" : "");
         }
         else
-        {}
+        {
+            std::size_t min_argc = std::numeric_limits<std::size_t>::max(), max_argc = 0;
+            for (const Contract& c : contracts)
+            {
+                if (c.arguments.size() < min_argc)
+                    min_argc = c.arguments.size();
+                if (c.arguments.size() > max_argc)
+                    max_argc = c.arguments.size();
+            }
 
-        throw Error("smh");  // TODO generate the error
+            std::cout << "between "
+                      << termcolor::yellow << min_argc << termcolor::reset
+                      << " argument" << (min_argc > 1 ? "s" : "") << " and "
+                      << termcolor::yellow << max_argc << termcolor::reset
+                      << " argument" << (max_argc > 1 ? "s" : "");
+        }
+        std::cout << " but got " << provided_arguments.size() << "\n";
+
+        displayContract(contracts[0], provided_arguments);
+        for (std::size_t i = 1, end = contracts.size(); i < end; ++i)
+        {
+            std::cout << "Alternative " << (i + 1) << ":\n";
+            displayContract(contracts[i], provided_arguments);
+        }
+
+        throw Error("");
     }
 }
