0f06bfa42052dd332596973d09a73dac204ae398
Alexandre Plateau <lexplt.dev@gmail.com>
2022-01-31 09:36:14 +0100

    fix: no more segfault when freeing usertypes

--- src/arkreactor/VM/Value/UserType.cpp
+++ src/arkreactor/VM/Value/UserType.cpp
@@ -2,6 +2,12 @@
 
 namespace Ark
 {
+    void UserType::del()
+    {
+        if (m_funcs && m_funcs->deleter)
+            m_funcs->deleter(m_data);
+    }
+
     bool operator==(const UserType& A, const UserType& B) noexcept
     {
         return (A.m_type_id == B.m_type_id) && (A.m_data == B.m_data);
