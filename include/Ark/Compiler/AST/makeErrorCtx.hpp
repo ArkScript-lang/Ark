163b0538424729b9781e5abe24d8ea230c1b368e
Robert Stefanic <rstefanic72@gmail.com>
2022-02-18 20:54:57 -0500

    Errors: Add colors to string output from makeContext
    From the Termcolor documentation, Termcolor ignores colors for non-tty
    streams. We can override this behavior by adding `termcolor::colorize`
    to the stream in order to enable.
    
    This commit adds a green color to the number lines and adds a red color
    the the `^`s in error contexts.
    
    
    Former-commit-id: 7f1d7430a399cab61d1a71dca075ce786aac36cc

--- include/Ark/Compiler/AST/makeErrorCtx.hpp
+++ include/Ark/Compiler/AST/makeErrorCtx.hpp
@@ -4,9 +4,9 @@
  * @brief Create string error context for AST errors
  * @version 0.1
  * @date 2020-10-27
- * 
+ *
  * @copyright Copyright (c) 2020-2021
- * 
+ *
  */
 
 #ifndef COMPILER_AST_MAKEERRORCTX_HPP
@@ -16,6 +16,7 @@
 #include <string>
 
 #include <Ark/Compiler/AST/Node.hpp>
+#include <termcolor/termcolor.hpp>
 
 namespace Ark::internal
 {
@@ -23,9 +24,9 @@ namespace Ark::internal
      * @brief Construct an error message based on a given node
      * @details It opens the related file at the line and column of the node,
      *          and display context, plus underline the problem with a serie of ^.
-     * 
-     * @param message 
-     * @param node 
+     *
+     * @param message
+     * @param node
      * @return std::string the complete generated error message
      */
     std::string makeNodeBasedErrorCtx(const std::string& message, const Node& node);
@@ -33,7 +34,7 @@ namespace Ark::internal
     /**
      * @brief Construct an error message based on a given match in the code
      * @details Mostly used by the Lexer and Parser since they don't have Nodes to work on
-     * 
+     *
      * @param match the identified token, causing a problem
      * @param line line of the token
      * @param col starting column of the token
