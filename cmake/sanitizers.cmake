244d0c26bf041d24e2001ceb02363399027b174e
Alexandre Plateau <lexplt.dev@gmail.com>
2022-07-17 17:44:37 +0200

    ci: disabling valgrind builds on GCC
    Can not fix the ODR violation on GCC for now.
    
    
    Former-commit-id: 85755f48e428f458d105b15e58c9a589117a41d0

--- cmake/sanitizers.cmake
+++ cmake/sanitizers.cmake
@@ -30,18 +30,21 @@ macro(add_sanitizer flag)
     include(CheckCXXCompilerFlag)
     if (CMAKE_CXX_COMPILER_ID MATCHES "Clang" OR CMAKE_CXX_COMPILER_ID STREQUAL "GNU" OR CMAKE_CXX_COMPILER_ID STREQUAL "AppleClang")
         message("Looking for -fsanitize=${flag}")
+
         set(CMAKE_REQUIRED_FLAGS "-Werror -fsanitize=${flag}")
         check_cxx_compiler_flag(-fsanitize=${flag} HAVE_FLAG_SANITIZER)
+
         if (HAVE_FLAG_SANITIZER)
             message("Adding -fsanitize=${flag}")
+
             set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -fsanitize=${flag} -fno-omit-frame-pointer")
             set(DCMAKE_C_FLAGS "${DCMAKE_C_FLAGS} -fsanitize=${flag} -fno-omit-frame-pointer")
             set(CMAKE_EXE_LINKER_FLAGS "${CMAKE_EXE_LINKER_FLAGS} -fsanitize=${flag}")
             set(DCMAKE_MODULE_LINKER_FLAGS "${DCMAKE_MODULE_LINKER_FLAGS} -fsanitize=${flag}")
-        else ()
+        else()
             message("-fsanitize=${flag} unavailable")
-        endif ()
-    endif ()
+        endif()
+    endif()
     #]
 endmacro()
 
