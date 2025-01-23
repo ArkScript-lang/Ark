5cb937ed3050fe145e86ff502f8c825578e6d33f
Alexandre Plateau <lexplt.dev@gmail.com>
2025-01-23 19:49:20 +0100

    feat(tests, repl): adding repl utils test suite

---
+++ tests/unittests/ReplSuite.cpp
@@ -0,0 +1,38 @@
+#include <boost/ut.hpp>
+
+#include <CLI/REPL/Utils.hpp>
+
+using namespace boost;
+
+ut::suite<"Repl"> repl_suite = [] {
+    using namespace ut;
+
+    "countOpenEnclosures"_test = [] {
+        expect(that % Ark::internal::countOpenEnclosures("", '(', ')') == 0);
+        expect(that % Ark::internal::countOpenEnclosures("(", '(', ')') == 1);
+        expect(that % Ark::internal::countOpenEnclosures(")", '(', ')') == -1);
+        expect(that % Ark::internal::countOpenEnclosures("{}", '(', ')') == 0);
+        expect(that % Ark::internal::countOpenEnclosures("{)(()}", '(', ')') == 0);
+        expect(that % Ark::internal::countOpenEnclosures("{)(()}", '{', '}') == 0);
+    };
+
+    "trimWhitespace"_test = [] {
+        const std::string expected = "hello world";
+        std::string line = expected;
+
+        Ark::internal::trimWhitespace(line);
+        expect(that % line == expected);
+
+        line = "  \thello world";
+        Ark::internal::trimWhitespace(line);
+        expect(that % line == expected);
+
+        line = "hello world  \t";
+        Ark::internal::trimWhitespace(line);
+        expect(that % line == expected);
+
+        line = "  \thello world  \t";
+        Ark::internal::trimWhitespace(line);
+        expect(that % line == expected);
+    };
+};
