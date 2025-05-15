8acac79899c50735125fffced0b3986e910c71e1
Alexandre Plateau <lexplt.dev@gmail.com>
2025-05-15 21:12:03 +0200

    feat(tools): adding more commands to fuzzer-crash-triage.py script
    r to run the code again
    x to show the source

--- tests/fuzzing/fuzzer-crash-triage.py
+++ tests/fuzzing/fuzzer-crash-triage.py
@@ -2,19 +2,54 @@
 import sys
 import os
 
+arkscript = None
+if os.path.exists("cmake-build-debug/arkscript"):
+    arkscript = "cmake-build-debug/arkscript"
+elif os.path.exists("build/arkscript"):
+    arkscript = "build/arkscript"
+else:
+    print("Couldn't find a suitable arkscript executable")
+    sys.exit(1)
+
+if not os.path.exists("fct-ok"):
+    os.mkdir("fct-ok")
+if not os.path.exists("fct-bad"):
+    os.mkdir("fct-bad")
+
+
+def run_file(file: str):
+    print(f"{file}\n{'-' * len(file)}")
+    exit_code = os.system(f"{arkscript} \"{file}\"")
+    print(f"\nEXITCODE: {exit_code}\n\n")
+
 
 def handle_command(i: int, count: int, file: str):
-    command = input(f"[{i}/{count}] o,c,s,q,? > ").strip().lower()
+    command = input(f"[{i}/{count}] o,c,r,x,s,q,? > ").strip().lower()
     if command == "o":
         os.rename(file, f"fct-ok/{os.path.basename(file)}")
     elif command == "c":
         os.rename(file, f"fct-bad/{os.path.basename(file)}")
+    elif command == "r":
+        run_file(file)
+        return True
+    elif command == "x":
+        with open(file) as f:
+            print(f.read())
+        return True
     elif command == "s":
         pass
     elif command == "q":
         sys.exit(0)
     elif command == "?":
-        print("\to: ok, not a crash\n\tc: crash\n\ts: skip\n\tq: quit\n\t?: this help message")
+        messages = [
+            "o: ok, not a crash",
+            "c: crash",
+            "r: run again",
+            "x: show source code",
+            "s: skip",
+            "q: quit",
+            "?: this help message"]
+        print("\t" + "\n\t".join(messages))
         return True
     else:
         print("Unknown command.")
@@ -23,20 +58,6 @@ def handle_command(i: int, count: int, file: str):
 
 
 def main():
-    arkscript = None
-    if os.path.exists("cmake-build-debug/arkscript"):
-        arkscript = "cmake-build-debug/arkscript"
-    elif os.path.exists("build/arkscript"):
-        arkscript = "build/arkscript"
-    else:
-        print("Couldn't find a suitable arkscript executable")
-        sys.exit(1)
-
-    if not os.path.exists("fct-ok"):
-        os.mkdir("fct-ok")
-    if not os.path.exists("fct-bad"):
-        os.mkdir("fct-bad")
-
     args = [f for f in sys.argv[1:] if "__arkscript__" not in f]
     files_count = len(args)
 
@@ -46,9 +67,7 @@ def main():
 
     for i, file in enumerate(args):
         if os.path.exists(file) and os.path.isfile(file):
-            print(f"{file}\n{'-' * len(file)}")
-            os.system(f"{arkscript} \"{file}\"")
-            print("\n\n")
+            run_file(file)
             while handle_command(i + 1, files_count, file):
                 pass
             print()
