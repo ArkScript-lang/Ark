a2159c5d4a8737d73fadcd81cce2347c24570dd3
Alexandre Plateau <lexplt.dev@gmail.com>
2025-12-05 13:07:35 +0100

    feat(parser): adding parsing of arguments' attributes in function declarations

--- include/Ark/Compiler/Common.hpp
+++ include/Ark/Compiler/Common.hpp
@@ -43,6 +43,8 @@ namespace Ark::internal
     enum class NodeType
     {
         Symbol,
+        MutArg,
+        RefArg,
         Capture,
         Keyword,
         String,
@@ -56,8 +58,10 @@ namespace Ark::internal
     };
 
     /// Node types as string, in the same order as the enum NodeType
-    constexpr std::array<std::string_view, 11> nodeTypes = {
+    constexpr std::array<std::string_view, 13> nodeTypes = {
         "Symbol",
+        "MutArg",
+        "RefArg",
         "Capture",
         "Keyword",
         "String",
