240104efc2918cad16471527c94ac4a8d81be764
Alexandre Plateau <lexplt.dev@gmail.com>
2025-12-11 15:02:06 +0100

    print files

--- tests/unittests/Main.cpp
+++ tests/unittests/Main.cpp
@@ -1,3 +1,6 @@
+#include "TestsHelper.hpp"
+
+
 #include <boost/ut.hpp>
 #include <fmt/format.h>
 #include <fmt/chrono.h>
@@ -17,6 +20,10 @@ int main(const int argc, char** argv)
     std::string filter = "*";
     if (argc >= 2 && std::string(argv[1]) != "update")
         filter = argv[1];
+
+    const auto path = getResourcePath("DiagnosticsSuite/runtime");
+    for (const auto& entry : std::filesystem::directory_iterator(path))
+        std::cout << "# " << entry.path().generic_string() << std::endl;
 
     bool failed = false;
     const auto start = std::chrono::high_resolution_clock::now();
