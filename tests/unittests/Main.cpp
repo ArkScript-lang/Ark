d01d28d6baaae34357471c7d495b05a3aadc5eb2
Alexandre Plateau <lexplt.dev@gmail.com>
2025-12-11 20:05:59 +0100

    what am i even doing

--- tests/unittests/Main.cpp
+++ tests/unittests/Main.cpp
@@ -1,6 +1,3 @@
-#include "TestsHelper.hpp"
-
-
 #include <boost/ut.hpp>
 #include <fmt/format.h>
 #include <fmt/chrono.h>
@@ -9,6 +6,31 @@
 
 #include <string>
 #include <chrono>
+
+void tt(const std::string& folder, std::function<void(const std::string&)>&& run)
+{
+    boost::ut::test(folder) = [=] {
+        for (const std::string& name : {
+            "11111111111111111111111111111111111111111111111111111111",
+            "2222222222222222222222222222222222222222222222222222222222",
+            "2222222222222222222222222222222222222222222222222222222222",
+            "333333333333333333333333333333333333333333333333333333333333"})
+        {
+            run(name);
+        }
+    };
+}
+
+boost::ut::suite<"Main"> main_suite = [] {
+    using namespace boost::ut;
+
+    tt("main", [](const std::string& name) {
+        should("parametrized " + name) = [] {
+            expect(true);
+            expect(false);
+        };
+    });
+};
 
 int main(const int argc, char** argv)
 {
@@ -20,15 +42,6 @@ int main(const int argc, char** argv)
     std::string filter = "*";
     if (argc >= 2 && std::string(argv[1]) != "update")
         filter = argv[1];
-
-    auto show = [](const std::string& res) {
-        const auto path = getResourcePath(res);
-        for (const auto& entry : std::filesystem::directory_iterator(path))
-            std::cout << "# " << entry.path().generic_string() << std::endl;
-    };
-    show("DiagnosticsSuite/compileTime");
-    show("DiagnosticsSuite/runtime");
-    show("DiagnosticsSuite/typeChecking");
 
     bool failed = false;
     const auto start = std::chrono::high_resolution_clock::now();
--- tests/unittests/TestsHelper.cpp
+++ tests/unittests/TestsHelper.cpp
@@ -58,7 +58,6 @@ void iterTestFiles(const std::string& folder, std::function<void(const TestData&
                 .is_folder = is_directory(entry.path())
             };
 
-            std::cout << "test on '" << entry.path().generic_string() << "'" << std::endl;
             test(data);
         }
     };
