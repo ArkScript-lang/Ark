ea7364b59bbf36864007c6a3f8ff4a0f9c496fd8
Lexy Plt <lexplt.dev@gmail.com>
2026-01-01 13:48:16 +0100

    chore(tests): add more tests for Ark::Utils::

--- tests/unittests/Suites/ToolsSuite.cpp
+++ tests/unittests/Suites/ToolsSuite.cpp
@@ -9,6 +9,28 @@ using namespace boost;
 ut::suite<"Tools"> tools_suite = [] {
     using namespace ut;
     using namespace Ark::literals;
+    using namespace std::string_literals;
+
+    "Utils::splitString"_test = [] {
+        expect(that % Ark::Utils::splitString("a,b,c", ',') == std::vector<std::string> { "a", "b", "c" });
+        expect(that % Ark::Utils::splitString("a,b,c", ';') == std::vector<std::string> { "a,b,c" });
+    };
+
+    "Utils::ltrim, Utils::rtrim"_test = [] {
+        std::string input = "abc";
+        expect(that % Ark::Utils::ltrim(input) == "abc"s);
+        input = "  abc";
+        expect(that % Ark::Utils::ltrim(input) == "abc"s);
+        input = "abc  ";
+        expect(that % Ark::Utils::ltrim(input) == "abc  "s);
+
+        input = "abc";
+        expect(that % Ark::Utils::rtrim(input) == "abc"s);
+        input = "  abc";
+        expect(that % Ark::Utils::rtrim(input) == "  abc"s);
+        input = "abc  ";
+        expect(that % Ark::Utils::rtrim(input) == "abc"s);
+    };
 
     "Utils::levenshteinDistance"_test = [] {
         expect(that % Ark::Utils::levenshteinDistance("arkscript", "arkscript") == 0_z);
