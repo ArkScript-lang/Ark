a172dc4e76c30efc95629f080797f506766f2284
Lexy Plt <lexplt.dev@gmail.com>
2026-03-20 14:32:08 +0100

    feat(cmake): allow MinSizeRel to use LTO

--- cmake/link_time_optimization.cmake
+++ cmake/link_time_optimization.cmake
@@ -3,8 +3,8 @@ include(CheckIPOSupported)
 check_ipo_supported(RESULT ipo_supported)
 
 function(enable_lto target_name)
-    if (ipo_supported AND (${CMAKE_BUILD_TYPE} STREQUAL "Release"))
-        message(STATUS "LTO enabled")
+    if (ipo_supported AND ((${CMAKE_BUILD_TYPE} STREQUAL "Release") OR (${CMAKE_BUILD_TYPE} STREQUAL "MinSizeRel")))
+        message(STATUS "LTO enabled for ${target_name}")
         set_target_properties(${target_name} PROPERTIES
                 INTERPROCEDURAL_OPTIMIZATION TRUE)
     else ()
