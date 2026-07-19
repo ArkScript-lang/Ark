766e2d49cd4926c8d0b52fad49793552d017d2ad
Lexy Plt <lexy@elranet.eu>
2026-07-19 13:02:54 +0200

    feat(tests): duplicate lang suite to run with and without the ir inliner

--- tests/unittests/Suites/LangSuite.cpp
+++ tests/unittests/Suites/LangSuite.cpp
@@ -7,58 +7,57 @@
 
 using namespace boost;
 
+void testResource(Ark::State& state, const bool is_res, const std::string& path, const uint16_t features)
+{
+    using namespace ut;
+
+    try
+    {
+        const bool ok = mut(state).doFile(is_res ? getResourcePath(path) : path, features);
+        expect(ok) << fatal << "compilation failed";
+    }
+    catch (const Ark::CodeError&)
+    {
+        expect(false) << fatal << "encountered an exception while compiling";
+    }
+
+    Ark::VM vm(state);
+    should("return exit code 0") = [&] {
+        expect(mut(vm).run() == 0_i);
+    };
+
+    should("have no failures") = [&] {
+        const auto failure_count = mut(vm).operator[]("failure_count");
+        expect(failure_count.valueType() == Ark::ValueType::Number &&
+               failure_count.number() == 0.0)
+            << "failure_count = 0\n";
+    };
+}
+
 ut::suite<"Lang"> lang_suite = [] {
     using namespace ut;
 
-    "[run arkscript unittests]"_test = [] {
-        Ark::State state({ lib_path, unittests_path });
-
-        try
-        {
-            const bool ok = mut(state).doFile(getResourcePath("LangSuite/unittests.ark"));
-            expect(ok) << fatal << "compilation failed";
-        }
-        catch (const Ark::CodeError&)
-        {
-            expect(false) << fatal << "encountered an exception while compiling";
-        }
-
-        Ark::VM vm(state);
-        should("return exit code 0") = [&] {
-            expect(mut(vm).run() == 0_i);
+    "run arkscript unittests"_test = [] {
+        "without IR inliner"_test = [] {
+            Ark::State state({ lib_path, unittests_path });
+            testResource(state, /* is_res= */ true, "LangSuite/unittests.ark", Ark::DefaultFeatures & ~Ark::FeatureIRInliner);
         };
 
-        should("have no failures") = [&] {
-            const auto failure_count = mut(vm).operator[]("failure_count");
-            expect(failure_count.valueType() == Ark::ValueType::Number &&
-                   failure_count.number() == 0.0)
-                << "failure_count = 0\n";
+        "with IR inliner"_test = [] {
+            Ark::State state({ lib_path, unittests_path });
+            testResource(state, /* is_res= */ true, "LangSuite/unittests.ark", Ark::DefaultFeatures);
         };
     };
 
-    "[run arkscript stdlib unittests]"_test = [] {
-        Ark::State state({ lib_path });
-
-        try
-        {
-            const bool ok = mut(state).doFile(ARK_TESTS_ROOT "lib/std/tests/all.ark");
-            expect(ok) << fatal << "compilation failed";
-        }
-        catch (const Ark::CodeError&)
-        {
-            expect(false) << fatal << "encountered an exception while compiling";
-        }
-
-        Ark::VM vm(state);
-        should("return exit code 0") = [&] {
-            expect(mut(vm).run() == 0_i);
+    "run arkscript stdlib unittests"_test = [] {
+        "without IR inliner"_test = [] {
+            Ark::State state({ lib_path });
+            testResource(state, /* is_res= */ false, ARK_TESTS_ROOT "lib/std/tests/all.ark", Ark::DefaultFeatures & ~Ark::FeatureIRInliner);
         };
 
-        should("have no failures") = [&] {
-            const auto failure_count = mut(vm).operator[]("failure_count");
-            expect(failure_count.valueType() == Ark::ValueType::Number &&
-                   failure_count.number() == 0.0)
-                << "failure_count = 0\n";
+        "with IR inliner"_test = [] {
+            Ark::State state({ lib_path });
+            testResource(state, /* is_res= */ false, ARK_TESTS_ROOT "lib/std/tests/all.ark", Ark::DefaultFeatures);
         };
     };
 };
